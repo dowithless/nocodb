b56a29f5a963a58c8603606ed6fd69e0063017bb
Fendy Heryanto <fendy@nocodb.com>
2025-06-18 05:49:30 +0000

    dataRead

--- packages/nocodb/src/helpers/getAst.ts
+++ packages/nocodb/src/helpers/getAst.ts
@@ -50,6 +50,7 @@ const getAst = async (
     apiVersion = NcApiVersion.V2,
     extractOrderColumn = false,
     includeSortAndFilterColumns = false,
+    skipSubstitutingColumnIds = false,
   }: {
     query?: RequestQuery;
     extractOnlyPrimaries?: boolean;
@@ -64,6 +65,7 @@ const getAst = async (
     apiVersion?: NcApiVersion;
     extractOrderColumn?: boolean;
     includeSortAndFilterColumns?: boolean;
+    skipSubstitutingColumnIds?: boolean;
   },
 ): Promise<{
   ast: Ast;
@@ -73,6 +75,10 @@ const getAst = async (
   // set default values of dependencyFields and nested
   dependencyFields.nested = dependencyFields.nested || {};
   dependencyFields.fieldsSet = dependencyFields.fieldsSet || new Set();
+
+  const getFieldKey = (col: Column) => {
+    return skipSubstitutingColumnIds ? col.id : col.title;
+  };
 
   let coverImageId;
   let dependencyFieldsForCalenderView;
@@ -114,9 +120,12 @@ const getAst = async (
   if (extractOnlyPrimaries) {
     const ast: Ast = {
       ...(model.primaryKeys
-        ? model.primaryKeys.reduce((o, pk) => ({ ...o, [pk.title]: 1 }), {})
+        ? model.primaryKeys.reduce(
+            (o, pk) => ({ ...o, [getFieldKey(pk)]: 1 }),
+            {},
+          )
         : {}),
-      ...(model.displayValue ? { [model.displayValue.title]: 1 } : {}),
+      ...(model.displayValue ? { [getFieldKey(model.displayValue)]: 1 } : {}),
     };
     await Promise.all(
       model.primaryKeys.map((c) =>
@@ -133,7 +142,7 @@ const getAst = async (
     const ast: Ast = {
       ...(dependencyFieldsForCalenderView || []).reduce((o, f) => {
         const col = model.columns.find((c) => c.id === f);
-        return { ...o, [col.title]: 1 };
+        return { ...o, [getFieldKey(col)]: 1 };
       }, {}),
     };
 
@@ -198,6 +207,7 @@ const getAst = async (
 
   const ast: Ast = await columns.reduce(async (obj, col: Column) => {
     let value: number | boolean | { [key: string]: any } = 1;
+    // TODO: also get from col.id
     const nestedFields =
       query?.nested?.[col.title]?.fields || query?.nested?.[col.title]?.f;
     if (nestedFields && nestedFields !== '*') {
@@ -314,7 +324,7 @@ const getAst = async (
 
     return {
       ...(await obj),
-      [col.title]: isRequested,
+      [getFieldKey(col)]: isRequested,
     };
   }, Promise.resolve({}));
 
